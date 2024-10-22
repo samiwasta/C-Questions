b640552553f48a4c24fafdc29376e8707d12799e
samiwasta <samiwasta.11@gmail.com>
2024-10-22 15:16:17 +0530

    21 Day - Pattern 9 Updated

--- Day-21/pattern8.c
+++ Day-21/pattern8.c
@@ -26,3 +26,21 @@ int main(){
 
     return 0;
 }
+
+// #include<stdio.h>
+// int main(){
+//     int num, k, val;
+//     scanf("%d", &num);
+
+//     for(int row = 1; row <= num; row++){
+//         k = num -1;
+//         val = row;
+//         for(int col = 1; col <= row; col++){
+//             printf("%d ", val);
+
+//             val += k--;
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
