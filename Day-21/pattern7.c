752dec090ff35b1d54bace778593c3cebdff87d6
samiwasta <samiwasta.11@gmail.com>
2024-10-23 07:44:11 +0530

    Day 21 Pattern 7 corrected

--- Day-21/pattern7.c
+++ Day-21/pattern7.c
@@ -11,17 +11,19 @@
 #include<stdio.h>
 int main(){
     int num, oddDigit=1;
+    
     scanf("%d", &num);
-
+    
     for(int row = 1; row <= num; row++){
-        
         for(int col = 1; col <= row; col++){
-            printf("%d ", oddDigit);
-            oddDigit += 2;
+            if(row == 1 && col == 1){
+                printf("%d ", 1);
+            }
+            else{
+               printf("%d ", oddDigit+=2); 
+            }
         }
-        
         oddDigit = 2 * row - 1;
-
         printf("\n");
     }
     return 0;
