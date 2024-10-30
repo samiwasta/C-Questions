9e9f3e7a88d038f3d3c413f3be92e3b5fb15b520
samiwasta <samiwasta.11@gmail.com>
2024-10-30 10:32:40 +0530

    Updated

--- Day-26/rotated_left_array.c
+++ Day-26/rotated_left_array.c
@@ -2,9 +2,16 @@
 int main(){
     int arr[3] = {1,2,3};
 
-    int rotated[3] = {arr[1], arr[2], arr[0]};
+    int temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = arr[2];
+    arr[2] = temp;
 
-    printf("Rotated left: %d %d %d", rotated[0], rotated[1], rotated[2]);
+    printf("Rotated left: ");
+
+    for(int i = 0; i < 3; i++){
+        printf("%d ", &arr[i]);
+    }
 
     return 0;
 }
